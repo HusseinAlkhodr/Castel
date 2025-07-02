f30c7a7afbd816ad1f72da5bce0898568fd3b8a7
Hussein-Alkhodr <hussein.alkhodr@meta-itech.com>
2025-07-02 23:51:04 +0300

    Add Bootstrap

--- Castel/obj/Debug/net8.0/Castel.AssemblyInfo.cs
+++ Castel/obj/Debug/net8.0/Castel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Castel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f44efe82fd23c0784f997cd80e903ec4365df48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35e5de7c52acd2e07666dbebad329ec2fb6a2a9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Castel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Castel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
