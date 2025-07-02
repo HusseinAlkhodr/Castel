1f44efe82fd23c0784f997cd80e903ec4365df48
Hussein-Alkhodr <hussein.alkhodr@meta-itech.com>
2025-07-02 19:24:54 +0300

    Add-View

--- Castel/obj/Debug/net8.0/Castel.AssemblyInfo.cs
+++ Castel/obj/Debug/net8.0/Castel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Castel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ca2b5564a6881abb975a3f561a715e4b838e974")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+383bdaec13393a5d520954d200c36b3514ed5b56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Castel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Castel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
